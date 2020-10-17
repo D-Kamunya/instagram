ede4c5dd0a272c38db64e024685fa9a0e2f455f7
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-17 23:33:59 +0300

    Add styling to registration templates

--- users/forms.py
+++ users/forms.py
@@ -4,9 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, help_text='Last Name')
-    last_name = forms.CharField(max_length=100, help_text='Last Name')
-
+    first_name = forms.CharField(label=False, widget=forms.TextInput(attrs={"class":"form-control",
+                                                               "placeholder":"First Name"}))
+    last_name = forms.CharField(label=False, widget=forms.TextInput(attrs={"class":"form-control",
+                                                               "placeholder":"Last Name"}))
+    username = forms.CharField(label=False, widget=forms.TextInput(attrs={"class":"form-control",
+                                                               "placeholder":"Username"}))
+    email = forms.CharField(label=False, widget=forms.TextInput(attrs={"class":"form-control",
+                                                               "placeholder":"Email"}))
+    password1 = forms.CharField(label=False, widget=forms.PasswordInput(attrs={"class":"form-control",
+                                                               "placeholder":"Password"}))
+    password2 = forms.CharField(label=False, widget=forms.PasswordInput(attrs={"class":"form-control",
+                                                               "placeholder":"Confirm Password"}))
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name','email', 'password1', 'password2',)
