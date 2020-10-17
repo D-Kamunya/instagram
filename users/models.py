cff2efa354739f8855b30bd3e9fd612250b07afa
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-17 14:34:42 +0300

    Add update profile signal receiver fn to create profile instance on user registration

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from pyuploadcare.dj.models import ImageField
 import datetime as dt
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -44,3 +46,10 @@ class Profile(models.Model):
       '''
       cls.objects.get(user_id=userid).delete()    
 
+
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
