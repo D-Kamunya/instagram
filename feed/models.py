71c4b2245384e619dc5169aa4428a1f2bfe370f4
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-18 23:20:33 +0300

    Create Image feed model class schema

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+import datetime as dt
+from users.models import Profile
 
-# Create your models here.
+class Image(models.Model):
+    """
+    Image class to define Image Objects
+    """
+    image_name = models.CharField(max_length =150)
+    image_path = ImageField(blank=True, manual_crop="")
+    image_caption = models.CharField(max_length =255)
+    profile = models.ForeignKey(Profile,
+    on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    comments=models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.image_name 
+
+
+    class Meta:
+        ordering = ['upload_date'] 
+
+    def save_image(self):
+      '''
+      Saves image instance to db
+      '''
+      self.save()
+
+
+    @classmethod
+    def get_all_images(cls):
+      '''
+      Returns all image objects from db
+      '''
+      images=cls.objects.all()
+      return images 
+
+
+    @classmethod
+    def get_image_by_id(cls,id):
+      '''
+      Returns image based on its id
+      '''
+      image=cls.objects.get(id=id)
+      return image
+
+
+
+    @classmethod
+    def delete_image(cls,id):
+      '''
+      Deletes image based on its id
+      '''
+      cls.objects.filter(id=id).delete()
+      
+
+    @classmethod
+    def search_image(cls,image_name):
+      '''
+      Allows us to search for an image using its namw.
+      '''
+      images=cls.objects.filter(image_name__icontains=image_name)
+      return images
+
+
+
+    @classmethod
+    def filter_by_userid(cls,user_id):
+      """
+      Allows us to filter images by the user posted.
+      """
+      images=cls.objects.filter(profile__user_id=user_id)
+      return images
+
--- feed/tests.py
+++ feed/tests.py
@@ -1,3 +1,67 @@
 from django.test import TestCase
-
+from .models import Image
+from django.contrib.auth.models import User
+from users.models import Profile
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        # Creating a new location and saving it
+        self.new_user=User(username='denno',email='a@gmail.com',password='qwerty1234')
+        self.new_user.save()
+        self.new_profile=Profile.get_profile_by_userid(self.new_user.id)
+        self.new_image= Image(image_name='Cycling',image_caption='sahgsbxsahzb',profile=self.new_profile)
+        self.new_image.save_image()
+
+
+
+    # Tear Down method
+    def tearDown(self):
+        Image.objects.all().delete()
+        Profile.objects.all().delete()
+        Image.objects.all().delete()      
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))    
+
+    # Testing Save Method
+    def test_save_method(self):
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)  
+
+    # Testing get all images Method
+    def test_get_all_images_method(self):
+        images = Image.get_all_images()
+        self.assertTrue(len(images) > 0) 
+
+
+    # Testing get all images Method
+    def test_get_image_by_id_method(self):
+        image = Image.get_image_by_id(self.new_image.id)
+        self.assertEqual(image.id,self.new_image.id)          
+
+
+    # Testing delete method
+    def test_delete_image(self):
+        Image.delete_image(self.new_image.id)
+        images = Image.get_all_images()
+        self.assertTrue(len(images) == 0)
+
+
+    # Testing search image by category method
+    def test_search_image(self):
+        images=Image.search_image('Cyc')
+        imagess=Image.search_image('Taa')
+        self.assertFalse(len(imagess) > 0)  
+        self.assertTrue(len(images) > 0)  
+
+
+    # Testing filter by userid method
+    def test_filter_by_userid(self):
+        images=Image.filter_by_userid(self.new_user.id)
+        self.assertTrue(len(images) > 0)
+
+
+       
