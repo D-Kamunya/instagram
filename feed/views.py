b38b02cef2b77f1e184da23418a5dccd0c16de44
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-18 14:48:34 +0300

    Create template's navbar html template

--- feed/views.py
+++ feed/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
+from users.models import Profile
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def home_page(request):
-
-
-  return render(request,'feed/home.html')
+  
+  profile=Profile.get_profile_by_userid(request.user.id)
+  print(profile.user.username)
+  return render(request,'feed/home.html',{'profile':profile})
