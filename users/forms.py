ce7b9c00aa68d9cf08aa776462c643f9df49c750
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-19 15:19:17 +0300

    Create user userprofile and editprofile models forms

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -19,3 +20,18 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name','email', 'password1', 'password2',)
+
+class EditProfileForm(forms.ModelForm):
+        class Meta:
+            model = User
+            fields = (
+                    'email',
+                    'first_name',
+                    'last_name'
+                    )
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user','bio']
+ 
