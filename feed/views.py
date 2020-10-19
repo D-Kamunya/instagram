f5d646c7d854be63dccc4bfc89712fd6d859b0a4
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-19 20:26:05 +0300

    Create logic to display post on feed home page

--- feed/views.py
+++ feed/views.py
@@ -2,15 +2,27 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 from users.models import Profile
+from .models import Image
 from .forms import NewImageForm
+from users import views as user_views
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def home_page(request):
-  
+  following=user_views.get_following(request)
+  following_posts=[]
+  all_posts=Image.get_all_images()
+  for post in all_posts:
+    if post.profile in following:
+      following_posts.append(post)
   profile=Profile.get_profile_by_userid(request.user.id)
+  my_posts=Image.filter_by_userid(request.user.id)
   context={
-    'profile':profile
+    'profile':profile,
+    'following':following,
+    'all_posts':all_posts,
+    'following_posts':following_posts,
+    'my_posts':my_posts
   }
   return render(request,'feed/home.html',context)
 
