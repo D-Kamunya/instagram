81a9e6fbe58b97e5770bc107a32ee8da839af119
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-20 12:12:16 +0300

    Create newcommentform model form

--- feed/forms.py
+++ feed/forms.py
@@ -1,9 +1,18 @@
 from django import forms
-from .models import Image
+from .models import Image,Image_Comment
 
 
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['profile', 'upload_date','likes','comments']
+
+
+
+class NewCommentForm(forms.ModelForm):
+
+	class Meta:
+		model = Image_Comment
+		fields = ['comment']
+
         
