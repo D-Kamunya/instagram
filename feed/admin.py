f55de87d9152bb213730cd875d667522f9f45372
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-20 22:30:36 +0300

    Add models to admin

--- feed/admin.py
+++ feed/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Image
+from .models import Image,Like,Image_Comment
 
 # Register your models here.
 admin.site.register(Image)
+admin.site.register(Like)
+admin.site.register(Image_Comment)
